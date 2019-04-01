0523f6d9218b2fa5be53c9cf3a977abb0807f916
hackanons <37089154+hackanons@users.noreply.github.com>
2019-04-02 02:15:19 +0530

    Add files via upload
    init python button click html outptut

---
+++ buttonpython/buttonpython/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import requests
+
+
+def button(request):
+
+    return render(request,'home.html')
+
+def output(request):
+    data=requests.get("https://www.google.com/")
+    print(data.text)
+    data=data.text
+    return render(request,'home.html',{'data':data})
